2f2cc9c2fd0214f6b7198496c7947780614d7a0c
Cristiano Ferreira <96497604+bitcris@users.noreply.github.com>
2023-07-21 17:00:36 -0300

    Update index.html

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-      $dados = $_SERVER;
-      //echo '<pre>';
-      $ip = isset($_SERVER["HTTP_X_REAL_IP"]) ? $_SERVER["HTTP_X_REAL_IP"] : "Indisponível";
-      $rota = isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : "Indisponível";
-      $proxy = isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : "Indisponível";
-      $plataforma = isset($_SERVER["HTTP_SEC_CH_UA_PLATFORM"]) ? $_SERVER["HTTP_SEC_CH_UA_PLATFORM"] : "Não identificado";
-      $cli = isset($_SERVER["HTTP_SEC_CH_UA"]) ? $_SERVER["HTTP_SEC_CH_UA"] : "Não identificado";
-
-      $elementos = explode(', "', $cli);
-      $cliente = array();
-      foreach ($elementos as $elemento) {
-          if (strpos($elemento, 'v=') !== false) {
-              $partes = explode('v=', $elemento);
-              $valor = trim(str_replace(['"', ';'], '', $partes[0]));
-              $cliente[] = $valor;
-          }
-      }
-
-      $req = file_get_contents('https://freeipapi.com/api/json/' . $ip);
-      $data = json_decode($req, true);
-
-      $cidade = $data["cityName"];
-      $pais = $data["countryName"];
-      $latitude = $data["latitude"];
-      $longitude = $data["longitude"];
-      
-?>
 
 <!DOCTYPE html>
 <html>
@@ -56,57 +27,13 @@ header("Access-Control-Allow-Origin: *");
 
   <div class="banner">
     <span>
-      <img src="meuipbann.png" alt="">
+      <img src="banner.png" alt="">
     </span>
   </div>
   
+
   
-  <div class="geral">
-    <div class="painel">
-      <?php
-      echo '<pre>';
-      echo "IP DE ORIGEM: $ip";
-      if($ip != $proxy){
-        echo "<br>ÚLTIMO NÓ: $proxy";
-      }
-      echo '</pre>'
-    ?>
 
-    <div id="local">
-      <?php
-      echo '<pre>';
-      echo "CIDADE: $cidade";
-      echo "<br>PAÍS: $pais";
-      echo '</pre>'
-      ?>
-    </div>  
-
-    <div class="dados">
-      <?php
-      echo '<pre>';
-      echo "SISTEMA OPERACIONAL: $plataforma";
-      echo "<br>CLIENTE: $cliente[2]";
-      echo '</pre>';
-      ?>
-    </div>
-  </div>
-  
-   <br><br>
-
-  <div id="map"></div>
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-  <script>
-
-    var latitude = "<?php echo $latitude; ?>";
-    var longitude = "<?php echo $longitude; ?>";
-    var map = L.map('map').setView([latitude, longitude], 5);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18,
-    }).addTo(map);
-    L.marker([latitude, longitude]).addTo(map);
-  </script>
-  <br>
 
    <div class="rodape">
     <pre>
